e642dca1d85d5116fe187c45a0e7d13d3ba0c804
shahin2525 <shahin451999@gmail.com>
2025-02-22 09:01:36 -0800

    add revalid

--- src/app/(commonLayout2)/page.js
+++ src/app/(commonLayout2)/page.js
@@ -3,22 +3,20 @@ import Image from "next/image";
 import React from "react";
 
 const HomePage = async () => {
-  const res = await fetch("http://localhost:5000/products/");
+  const res = await fetch("http://localhost:5000/products/", {
+    next: {
+      revalidate: 5,
+    },
+  });
   const products = await res.json();
   console.log("products", products);
   return (
     <div>
-      <Image
-        alt="home"
-        src="https://imgur.com/a/tGSksLH"
-        width={200}
-        height={200}
-      />
       <h1>i am from home page</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-        {/* {products.map((product) => (
+        {products.map((product) => (
           <ProductCard key={product.id} product={product} />
-        ))} */}
+        ))}
       </div>
     </div>
   );
