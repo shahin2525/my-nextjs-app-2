e34549283d06680468c579cd405a9c6e9eb649da
shahin2525 <shahin451999@gmail.com>
2025-02-22 20:14:56 -0800

    add server side render

--- src/app/(commonLayout2)/page.js
+++ src/app/(commonLayout2)/page.js
@@ -1,5 +1,6 @@
 import ProductCard from "@/components/ProductCard";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const HomePage = async () => {
@@ -14,10 +15,13 @@ const HomePage = async () => {
     <div>
       <h1>i am from home page</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-        {products.map((product) => (
+        {products.slice(0, 3).map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+      <Link className="my-4" rel="stylesheet" href="/products">
+        See More
+      </Link>
     </div>
   );
 };
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,11 +1,21 @@
+import ProductCard from "@/components/ProductCard";
 import React from "react";
 
-const ProductsPage = () => {
+const AllProducts = async () => {
+  const res = await fetch("http://localhost:5000/products/", {
+    cache: "no-store",
+  });
+  const products = await res.json();
   return (
     <div>
-      <h1>I am from Products page</h1>
+      <h1>All products</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default ProductsPage;
+export default AllProducts;
